9c25b83f46deecb1dfe54f0c7718c867e8ec13e8
Chaoseye <chaoseyex@gmail.com>
2026-02-07 03:05:07 +0100

    chore: exclude backup folder from build and git tracking

--- iBoosts-website-c7d0b0c7949f2572321ee56fd47b3bb120c95b33/src/components/layout/index.ts
+++
@@ -1,4 +0,0 @@
-export { MainNavbar } from "./main-navbar";
-export { Footer } from "./footer";
-export { Sidebar } from "./sidebar";
-export { DashboardSidebar } from "./dashboard-sidebar";
--- iBoosts-website-c7d0b0c7949f2572321ee56fd47b3bb120c95b33/src/components/ui/index.ts
+++
@@ -1,9 +0,0 @@
-// UI Components Barrel Export
-export { Button, buttonVariants } from "./button";
-export { Input } from "./input";
-export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "./card";
-export { Badge, badgeVariants } from "./badge";
-export { Avatar } from "./avatar";
-export { Modal, ModalFooter } from "./modal";
-export { Skeleton, SkeletonCard, SkeletonListItem, SkeletonTable } from "./skeleton";
-export { Select } from "./select";
